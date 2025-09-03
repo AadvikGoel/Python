b2779fd92dd888f09739b247736ba0a14f6cfb57
AadvikGoel <deepugoel5@gmail.com>
2025-09-03 17:13:24 +0530

    top.py

---
+++ Strings.py
@@ -0,0 +1,13 @@
+first_name = "Chris"
+last_name = "Gayle"
+full_name = first_name+last_name
+example = "Haa"*5
+print("First Name :", first_name)
+print("Last Name :", last_name)
+print("Full Name :", full_name)
+print("String Multiplied 5 times gives this result:", example)
+word = 'Cricket'
+print("Length of String:", len(word))
+print("First Letter of String:", word[5])
+print("Last Letter of String:", word[5])
+print("String Silenced:", word[0.3])
